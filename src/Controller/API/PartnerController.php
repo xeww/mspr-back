81bd9b3f71dcf401326e3c39296b744d2ae48ffc
François L.P. <me@francois-lp.dev>
2024-11-06 10:32:33 +0100

    feat: run php-cs-fixer

--- src/Controller/API/PartnerController.php
+++ src/Controller/API/PartnerController.php
@@ -15,25 +15,36 @@ use Symfony\Component\Serializer\SerializerInterface;
 class PartnerController extends AbstractController
 {
     #[Route("/", name: "partner_api_index", methods: ["GET"])]
-    public function index(PartnerRepository $repository) :Response{
+    public function index(PartnerRepository $repository): Response
+    {
         return $this->json($repository->findAll(), Response::HTTP_OK);
     }
 
     #[Route("/{id}", name: "partner_api_get", methods: ["GET"])]
-    public function get(Partner $entity):Response {
+    public function get(Partner $entity): Response
+    {
         return $this->json($entity, Response::HTTP_OK);
     }
 
     #[Route("/{id}", name: "partner_api_update", methods: ["PUT"])]
-    public function update(Partner $entity, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer) : Response {
+    public function update(Partner $entity, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): Response
+    {
         $requestData = $request->getContent();
         $updatedEntity = $serializer->deserialize($requestData, Partner::class, "json");
         $from = $serializer->serialize($entity, "json");
 
-        if($updatedEntity->getName()) $entity->setName($updatedEntity->getName());
-        if($updatedEntity->getType()) $entity->setType($updatedEntity->getType());
-        if($updatedEntity->getImageName()) $entity->setImageName($updatedEntity->getImageName());
-        if($updatedEntity->getImageFile()) $entity->setImageFile($updatedEntity->getImageFile());
+        if ($updatedEntity->getName()) {
+            $entity->setName($updatedEntity->getName());
+        }
+        if ($updatedEntity->getType()) {
+            $entity->setType($updatedEntity->getType());
+        }
+        if ($updatedEntity->getImageName()) {
+            $entity->setImageName($updatedEntity->getImageName());
+        }
+        if ($updatedEntity->getImageFile()) {
+            $entity->setImageFile($updatedEntity->getImageFile());
+        }
 
         $entityManager->flush();
 
@@ -47,7 +58,8 @@ class PartnerController extends AbstractController
     }
 
     #[Route("/{id}", name: "partner_api_delete", methods: ["DELETE"])]
-    public function delete(Partner $entity, EntityManagerInterface $entityManager) : Response {
+    public function delete(Partner $entity, EntityManagerInterface $entityManager): Response
+    {
         $entityManager->remove($entity);
         $entityManager->flush();
 
@@ -55,11 +67,12 @@ class PartnerController extends AbstractController
     }
 
     #[Route("/create", name: "partner_api_create", methods: ["POST"])]
-    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer):Response {
+    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): Response
+    {
         $requestData = $request->getContent();
         $entity = $serializer->deserialize($requestData, Partner::class, "json");
 
-        if(!$entity->getName() || !$entity->getType() || !$entity->getImageName() || !$entity->getImageUrl()) {
+        if (!$entity->getName() || !$entity->getType() || !$entity->getImageName() || !$entity->getImageUrl()) {
             return $this->json(["error" => "Missing required fields"], Response::HTTP_BAD_REQUEST);
         }
 

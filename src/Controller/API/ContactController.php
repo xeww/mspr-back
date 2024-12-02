0ba74be9db62e872b00798a88cea805d5ed5a917
François L.P. <me@francois-lp.dev>
2024-12-02 23:39:59 +0100

    feat: contact fix

--- src/Controller/API/ContactController.php
+++ src/Controller/API/ContactController.php
@@ -15,12 +15,6 @@ class ContactController extends AbstractController
     #[Route("/api/contact", name: "contact", methods: ["POST"])]
     public function send(Request $request, EntityManagerInterface $entityManager, RateLimiterFactory $contactApiLimiter): Response
     {
-        $limiter = $contactApiLimiter->create($request->getClientIp());
-
-        if (false === $limiter->consume(1)->isAccepted()) {
-            return $this->json(["error" => "Rate limit."], Response::HTTP_BAD_REQUEST);
-        }
-
         $content = json_decode($request->getContent(), true);
 
         $fullName = $content["fullName"] ?? null;
@@ -28,15 +22,21 @@ class ContactController extends AbstractController
         $message = $content["message"] ?? null;
 
         if ($fullName === null || strlen($fullName) >= 255) {
-            return $this->json(["error" => "Le nom/prénom est trop long ou est n'est pas renseigné."], Response::HTTP_BAD_REQUEST);
+            return $this->json(["error" => "Le nom/prénom est trop long ou n'est pas renseigné."], Response::HTTP_BAD_REQUEST);
         }
 
         if ($email === null || strlen($email) >= 255) {
-            return $this->json(["error" => "L'email est trop longue ou est n'est pas renseignée."], Response::HTTP_BAD_REQUEST);
+            return $this->json(["error" => "L'email est trop longue ou n'est pas renseignée."], Response::HTTP_BAD_REQUEST);
         }
 
         if ($message === null || strlen($message) >= 2000) {
             return $this->json(["error" => "Le message est trop long ou n'est pas renseigné."], Response::HTTP_BAD_REQUEST);
+        }
+
+        $limiter = $contactApiLimiter->create($request->getClientIp());
+
+        if (false === $limiter->consume(1)->isAccepted()) {
+            return $this->json(["error" => "Veuillez attendre quelques minutes avant de renvoyer un formulaire."], Response::HTTP_BAD_REQUEST);
         }
 
         $contact = new Contact();
